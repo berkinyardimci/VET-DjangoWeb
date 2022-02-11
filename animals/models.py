735acbf311731232e372eea07d188d2a43e0454f
= <=>
2022-02-11 04:21:59 +0300

    StartAppAnimals

---
+++ animals/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Animal(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
+    name = models.CharField(max_length=50)
+    type = models.CharField(max_length=30)
+    genus = models.CharField(max_length=30)
+    age = models.IntegerField()
+    available = models.BooleanField(default=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="courses/%Y/%m/%d/")
+    update = models.DateTimeField(auto_now=True)
